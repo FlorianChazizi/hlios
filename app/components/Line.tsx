f18108c64967994d7e6ee4c86c0a5416da6582c5
FlorianChazizi <chazizi.florian@gmail.com>
2025-01-12 00:31:47 +0200

    Line

---
+++ app/components/Line.tsx
@@ -0,0 +1,16 @@
+"use client"
+import React from 'react'
+import '../styles/line.css';
+
+const Line = () => {
+    return (
+        <div className='line'>
+            <div className='overlay'>
+                <div className='content'>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Line
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 import Hero from "./components/Hero";
 import AboutUs from "./components/AboutUs";
+import Line from "./components/Line";
 
 export default function Home() {
   return (
     <div>
-      
+
       <Hero />
       <AboutUs />
-
+      <Line />
+      
     </div>
   );
 }
